a45cd4d4297a99949a0a629a92e9f5d91d022740
IdoKawaz <idohaker@gmail.com>
2026-04-23 22:46:55 +0300

    feat: update Layout component padding and hide scrollbar buttons for improved layout aesthetics

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ import { Navbar } from './Navbar'
 export const Layout = () => (
   <div className="flex h-dvh flex-col overflow-hidden bg-background">
     <Navbar />
-    <main className="flex-1 min-h-0 overflow-y-auto px-4 py-8 sm:px-6 lg:px-10">
+    <main className="flex-1 min-h-0 overflow-y-auto px-4 pt-8 sm:px-6 lg:px-10">
       <Outlet />
     </main>
   </div>
